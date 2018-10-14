18fcc095beda539da18c3494c8f898dbf12e600c
dznqbit <quinton.harris@gmail.com>
2018-10-13 23:57:03 -0700

    Macdinking comment position

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,11 +1,11 @@
 # aliases.zsh
 alias resource="source ~/.zshrc"
 
-alias magerun="n98-magerun.phar" # PHP Magerun
+alias magerun="n98-magerun.phar"          # PHP Magerun
 
-alias ag=rg # Ripgrep 
-alias pg="ps aux | grep $1"
+alias ag=rg                               # Ripgrep 
+alias pg="ps aux | grep $1"               # List processes by name
 alias be="bundle exec"
 alias begs="bundle exec guard start"
 
-alias gcm="git commit -m $1"
+alias gcm="git commit -m $1"              # Git commit with message
