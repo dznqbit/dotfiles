5eaa2993146805b400367bb149e89a57ff8f822b
dznqbit <quinton.harris@gmail.com>
2018-10-13 23:53:35 -0700

    Add alias "pg <process-name>": ps aux | grep $1

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,11 @@
 # aliases.zsh
 alias resource="source ~/.zshrc"
 
-alias magerun="n98-magerun.phar"
-alias tksql="mysql -u $TOTOKAELO_DB_USER --password=$TOTOKAELO_DB_PASSWORD -h $TOTOKAELO_DB_HOST $TOTOKAELO_DB_DATABASE"
+alias magerun="n98-magerun.phar" # PHP Magerun
 
-alias ag=rg
+alias ag=rg # Ripgrep 
+alias pg="ps aux | grep $1"
 alias be="bundle exec"
 alias begs="bundle exec guard start"
+
+alias gcm="git commit -m $1"
